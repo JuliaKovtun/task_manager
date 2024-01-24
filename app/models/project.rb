3c44755f04466b171126b738ebb5dfd98d5d6f4c
Julia Kovtun <julia141407@gmail.com>
2024-01-24 21:06:24 +0200

    Added scopes to project and task models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   has_many :tasks
+
+  scope :with_tasks, -> { includes(:tasks) }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
-  enum status: { new: 0, in_progress: 1, done: 2 }
+  enum status: { new: 0, in_progress: 1, done: 2 }, _prefix: true
 
   belongs_to :project
+
+  scope :with_status, ->(status) { where(status: status) }
 end
