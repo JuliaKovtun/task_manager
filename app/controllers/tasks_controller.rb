04b93a46ba5a50f88401d4ceb7463fb4608b0d73
Julia Kovtun <julia141407@gmail.com>
2024-01-26 23:57:59 +0200

    Added tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[show update destroy]
+  before_action :set_project, only: :index
+
+  def index
+    @tasks = if params[:status]
+               @project.tasks.with_status(params[:status])
+             else
+               @project.tasks
+             end
+
+    render json: @tasks
+  end
+
+  def show
+    render json: @task
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  private
+
+  def task_params
+    permited_params = params.require(:task).permit(:title, :description, :status)
+    permited_params.merge({ project_id: params[:project_id] })
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+end
