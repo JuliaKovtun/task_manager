8bb242e79800c04cb13760ee7bad968210cd09fd
Julia Kovtun <julia141407@gmail.com>
2024-01-28 01:01:04 +0200

    Added devise, simple_token_authentication gems

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ gem "puma", ">= 5.0"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem "devise"
+gem "simple_token_authentication"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_171855) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_27_115328) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -26,6 +26,20 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_171855) do
     t.datetime "updated_at", null: false
     t.integer "project_id"
     t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
