b9b445b4ea3f66e4db1b4ea487270eb5e5046e90
Julia Kovtun <julia141407@gmail.com>
2024-01-29 16:22:35 +0200

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Creates a user
+User.create!(email: 'user@gmail.com', password: '123456', password_confirmation: '123456')
+
+# Creates an array of project parameters
+projects_params = (1..10).map do |number|
+  { title: "Project #{number}", description: "The description of project #{number}" }
+end
+
+# Creates projects with the parameters
+Project.create!(projects_params)
+
+# Creates tasks for the first 5 projects
+Project.first(5).each do |project|
+  tasks = [
+    { title: "New task for project #{project.title}", description: 'Task description', status: :new },
+    { title: "In progress task for project #{project.title}", description: 'Task description', status: :in_progress },
+    { title: "Done task for project #{project.title}", description: 'Task description', status: :done }
+  ]
+  project.tasks.create!(tasks)
+end
